4d1b4b5bdb9c3ef32a9a1af110518b58887a87a7
seaofocean <jiaxianyan@gmail.com>
2016-09-26 09:53:34 +0800

    update

---
+++ evaluate_test.py
@@ -0,0 +1,4 @@
+from pascal_voc import pascal_voc
+
+imdb = pascal_voc('test', '2007', '/home/test/xianyan/data/VOCdevkit')
+imdb._do_python_eval('results')
---
+++ run_yolo.sh
@@ -0,0 +1,1 @@
+python yolo_main.py -g 6 -m prototxt/yolo_small_deploy.prototxt -w model/yolo_small.caffemodel -i images 
